9c09750fa9b7f152a4a2ff9d5bebb6f426cab968
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-10-11 22:47:40 +0200

    Update dto's

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,11 +1,15 @@
 import { z } from 'zod';
 
-export const userIdSchema = z.string().min(1);
+export const userIdSchema = z
+  .string()
+  .min(1, { message: 'Valid user ID is required' });
 
 export const createUserSchema = z.object({
-  name: z.string().nullish(),
-  email: z.string().email(),
-  password: z.string().min(8),
+  name: z.string().min(1, { message: 'Invalid name' }).nullish(),
+  email: z.string().email({ message: 'Invalid email' }),
+  password: z
+    .string({ invalid_type_error: 'Invalid password' })
+    .min(8, { message: 'Password must be at least 8 characters long' }),
 });
 
 export type UserIdDto = z.infer<typeof userIdSchema>;
